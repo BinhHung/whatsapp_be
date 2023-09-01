b20c55d5668a00a0b0dc57a6dfe8559e6457e56f
BinhHung <binhhung2399@gmail.com>
2023-09-01 14:54:10 +0700

    setup cross-env, adding middlwares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+//dotEnv config
+dotenv.config();
+ 
 //create express app
 const app = express();
 
-app.post('/test',(req,res) => {
-    res.send("hello from server");
-});
+//Morgan
+if(process.env.NODE_ENV !== "production"){
+    app.use(morgan("dev"));
+}
+//Helmet
+app.use(helmet());
 
+//Parse json request url
+app.use(express.json());
+
+//Parse json request body
+app.use(express.urlencoded({extended: true}));
+
+app.post("/test", (req,res) => {
+    res.send(req.body);
+});
 
 export default app;
