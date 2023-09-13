53f71fddc9b34c102f0742e80d511da3b6bb5c0a
BinhHung <binhhung2399@gmail.com>
2023-09-13 12:57:30 +0700

    Login and logout functions

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -4,3 +4,7 @@ export const generateToken = async (payload, expiresIn, secret) => {
     let token = await sign(payload, expiresIn, secret);
     return token;
 }; 
+
+export const signUser = async (email, password) => {
+    
+}
